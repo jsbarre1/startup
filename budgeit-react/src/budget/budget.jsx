c0a74016fcfba066becf8b7c50cc7857cb40e484
John Barrett <john.bearett@gmail.com>
2025-02-14 17:17:11 -0700

    restructured files

---
+++ budgeit-react/src/budget/budget.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Budget() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>Budget displayed here</div>
+    </main>
+  );
+}
