8f948b98a13e3fb5cdad8848e3a1c5e0f9342794
John Barrett <john.bearett@gmail.com>
2025-02-14 17:54:50 -0700

    final touches to login

--- budgeit-react/src/login/login.jsx
+++ budgeit-react/src/login/login.jsx
@@ -10,9 +10,9 @@ export function Login() {
         <input className="mt-4  shadow-md align-middle text-center rounded-2xl lg:w-[400px] w-[200px] ring-2 ring-blue-400" type="text" placeholder="your@email.com" />
       </div>
       <div className="flex w-full justify-center">
-        <input className=" text-center shadow-md ring-2 rounded-2xl lg:w-[400px] w-[200px] ring-blue-400" type="password" placeholder="password" />
+        <input className=" text-center shadow-md ring-2 rounded-2xl lg:w-[400px] w-[200px] ring-blue-400 mt-2" type="password" placeholder="password" />
       </div>
-      <div className="flex flex-row justify-center">
+      <div className="flex flex-row justify-center mt-2">
         <button className="p-2 w-[100px] shadow-md lg:w-[200px] rounded-3xl bg-blue-500 text-white" type="submit">Login</button>
       <button className="p-2 w-[100px] shadow-md lg:w-[200px] ml-2 rounded-3xl bg-gray-500 text-white" type="submit">Create</button>
       </div>
