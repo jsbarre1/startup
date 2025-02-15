0a660746ec2f50a96013ee0b112a6860386acbca
John Barrett <john.bearett@gmail.com>
2025-02-14 17:47:53 -0700

    budget restored

--- budgeit-react/src/leaderboard/leaderboard.jsx
+++ budgeit-react/src/leaderboard/leaderboard.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React from "react";
 
 export function Leaderboard() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>leaderboard displayed here</div>
+    <main className="flex flex-col pt-6 w-full h-full justify-center">
+      <div className="text-xs justify-center">
+        <div className="bg-blue-300 text-center border border-white">
+          James recently scored 500 points!
+        </div>
+        <div className="bg-blue-300 text-center border border-white">
+          Brock recently scored 200 points!
+        </div>
+        <div className="bg-blue-300 text-center border border-white">
+          Aliza recently scored 50 points!
+        </div>
+        <div className="bg-blue-300 text-center border border-white">
+          (every time someone scores hit a random sound generator api)
+        </div>
+      </div>
+      <div className="flex flex-col bg-gray-200 self-center w-full md:w-[800px] rounded-lg shadow-md">
+        <h1 className="flex justify-center text-center">Leaderboard</h1>
+        <div className="flex flex-row justify-evenly bg-blue-400 rounded-2xl shadow-md">
+          <div className="w-full text-center">1</div>
+          <div className="w-full text-center">James</div>
+          <div className="w-full text-center">15075</div>
+        </div>
+        <div className="flex flex-row justify-evenly bg-blue-400 rounded-2xl mt-1 shadow-md">
+          <div className="w-full text-center">2</div>
+          <div className="w-full text-center">Brock</div>
+          <div className="w-full text-center">13000</div>
+        </div>
+        <div className="flex flex-row justify-evenly bg-blue-400 rounded-2xl mt-1 shadow-md">
+          <div className="w-full text-center">3</div>
+          <div className="w-full text-center">Aliza</div>
+          <div className="w-full text-center">12075</div>
+        </div>
+        <div className="flex flex-row justify-evenly bg-blue-400 rounded-2xl mt-1 shadow-md">
+          <div className="w-full text-center">4</div>
+          <div className="w-full text-center">(Username)</div>
+          <div className="w-full text-center">11000</div>
+        </div>
+      </div>
     </main>
   );
 }
--- budgeit-react/src/login/login.jsx
+++ budgeit-react/src/login/login.jsx
@@ -3,18 +3,18 @@ import React from 'react';
 export function Login() {
   return (
     
-    <main class="flex flex-col pt-6 w-full justify-center">
-    <h1 class="text-center">Welcome to BudgeIt</h1>
+    <main className="flex flex-col pt-6 w-full justify-center">
+    <h1 className="text-center">Welcome to BudgeIt</h1>
     <form method="get" action="budget.html">
-      <div class="flex w-full justify-center">
-        <input class="mt-4  shadow-md align-middle text-center rounded-2xl lg:w-[400px] w-[200px] ring-2 ring-blue-400" type="text" placeholder="your@email.com" />
+      <div className="flex w-full justify-center">
+        <input className="mt-4  shadow-md align-middle text-center rounded-2xl lg:w-[400px] w-[200px] ring-2 ring-blue-400" type="text" placeholder="your@email.com" />
       </div>
-      <div class="flex w-full justify-center">
-        <input class=" text-center shadow-md ring-2 rounded-2xl lg:w-[400px] w-[200px] ring-blue-400" type="password" placeholder="password" />
+      <div className="flex w-full justify-center">
+        <input className=" text-center shadow-md ring-2 rounded-2xl lg:w-[400px] w-[200px] ring-blue-400" type="password" placeholder="password" />
       </div>
-      <div class="flex flex-row justify-center">
-        <button class="p-2 w-[100px] shadow-md lg:w-[200px] rounded-3xl bg-blue-500 text-white" type="submit">Login</button>
-      <button class="p-2 w-[100px] shadow-md lg:w-[200px] ml-2 rounded-3xl bg-gray-500 text-white" type="submit">Create</button>
+      <div className="flex flex-row justify-center">
+        <button className="p-2 w-[100px] shadow-md lg:w-[200px] rounded-3xl bg-blue-500 text-white" type="submit">Login</button>
+      <button className="p-2 w-[100px] shadow-md lg:w-[200px] ml-2 rounded-3xl bg-gray-500 text-white" type="submit">Create</button>
       </div>
       
     </form>
